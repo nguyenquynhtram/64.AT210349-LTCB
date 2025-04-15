06610e411d8800308c7e14e68e8649a7685c9e9b
nguyenquynhtram <qtram1620@gmail.com>
2025-04-15 23:09:15 +0700

    Create b3.c

---
+++ con trỏ/b3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int in(int a[], int n){
+	for (int i=0; i<n; i++){
+		printf("%d ",a[i]);
+	}
+}
+void xep(int a[], int n){
+	for (int i=0; i<n; i++ ){
+		for (int j=i; j<n; j++){
+			if (a[i]>a[j]){
+				int p=a[i];
+				a[i]=a[j];
+				a[j]=p;
+			}
+		}
+	}
+}
+int main (){
+	int n;
+	printf("nhap so phan tu cua mang\n");
+	scanf("%d",&n);
+	int a[n];
+	printf("nhap mang gom %d phan tu\n",n);
+	for (int i=0; i<n; i++){
+		scanf("%d",&a[i]);
+	}
+	xep(a,n);
+	printf("min = %d\nmax = %d",a[0],a[n-1]);
+}
